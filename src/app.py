9426caec3a612667110ff3bb7170bda3b6fcd05d
Merlina Barbara Dowgaluk <merlina.dowgaluk@gmail.com>
2023-12-13 20:27:33 +0000

    feat: first commit

--- src/app.py
+++ src/app.py
@@ -25,18 +25,52 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/members', methods=['GET'])
-def handle_hello():
+@app.route('/members', methods=['GET','POST'])
+def handle_members():
+    response_body = {}
+    if request.method == 'GET':
+        # this is how you can use the Family datastructure by calling its methods
+        members = jackson_family.get_all_members()
+        response_body['message'] = 'Encontrado'
+        response_body['results'] = members
+        return response_body, 200
+    if request.method == 'POST':
+        data = request.json
+        print(data)
+        jackson_family.add_member(data)
+        members = jackson_family.get_all_members()
+        response_body['message'] = 'Recibido'
+        response_body['results'] = members
+        return response_body, 200
 
-    # this is how you can use the Family datastructure by calling its methods
-    members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
-
-
-    return jsonify(response_body), 200
+@app.route('/members/<int:id_member>', methods=['GET', 'DELETE', 'PUT'])
+def handle_member(id_member):
+    response_body = {}
+    if request.method == 'GET':
+        member = jackson_family.get_member(id_member)
+        if member:
+            response_body['message'] = 'Encontrado'
+            response_body['results'] = member
+            return response_body, 200
+        response_body['message'] = 'No encontrado'
+        response_body['results'] = []  
+        return response_body, 400
+    if request.method == 'DELETE':
+        member = jackson_family.delete_member(id_member)
+        if member:
+            response_body['message'] = 'Eliminado'
+            response_body['results'] = member
+            return response_body, 200
+        response_body['message'] = 'No encontrado'
+        response_body['results'] = []  
+        return response_body, 400
+    if request.method == 'PUT':
+        data = request.json
+        jackson_family.modify_member(data, id_member)
+        members = jackson_family.get_all_members()
+        response_body['message'] = 'Modificado'
+        response_body['results'] = members
+        return response_body, 200
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
